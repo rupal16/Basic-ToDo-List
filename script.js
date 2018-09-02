bde304940633aebad024d97f591f2844a95bc823
Rupal Jain <rupaljain.1699@gmail.com>
2018-09-02 20:19:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var todos = [];
+var input = prompt("What would you like to do?");
+
+while(input !== "quit") {
+	if(input === "list"){
+		console.log(todos);
+	}
+	else if(input === "new") {
+		var newtodo = prompt("Enter new todo : ");
+		todos.push(newtodo);
+	}
+   input = prompt("What would you like to do?");
+}
+
+console.log("OK!! YOU QUIT THE APP. ")
