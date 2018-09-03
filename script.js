a5135bbbaef9aee78cc59a0e85049eaf13f4bd9b
Rupal Jain <rupaljain.1699@gmail.com>
2018-09-03 12:35:15 +0530

    Add files via upload
    update

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
 	if(input === "list"){
-		console.log(todos);
+		console.log("************");
+		todos.forEach(function(todo,i){
+
+		console.log(i + " : " + todo);
+	});
+		console.log("************");
 	}
+
+	
+		
+	
 	else if(input === "new") {
 		var newtodo = prompt("Enter new todo : ");
 		todos.push(newtodo);
+		console.log("Added todo !");
 	}
+
+	else if(input === "delete") {
+		var index = prompt("Enter index of todo to delete : ");
+		todos.splice(index,1);
+		console.log("Deleted todo !");
+	}
+
    input = prompt("What would you like to do?");
 }
 
-console.log("OK!! YOU QUIT THE APP. ")
+console.log("OK!! YOU QUIT THE APP. ");
